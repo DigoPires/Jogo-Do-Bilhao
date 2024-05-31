5770672559c0527c57b5c7c612bc66e2faa99b19
DigoPires <rodrigo_pires2806@outlook.com>
2024-05-31 19:45:35 -0300

    Ajustes nas Perguntas

--- JogoDoBilhao.java
+++ JogoDoBilhao.java
@@ -312,9 +312,7 @@ public class JogoDoBilhao {
         
         perguntas.add(new PerguntaClass("Em que ano o homem pisou na Lua pela primeira vez? \n\na - 1965 \nb - 1969 \nc - 1972 \nd - 1975", "b", "DICA: A missão foi a Apollo 11.", "Em que ano o homem pisou na Lua pela primeira vez? \n\na - 1965 \nb - 1969", "a"));
         
-        perguntas.add(new PerguntaClass("Qual é a maior floresta tropical do mundo? \n\na - Floresta Amazônica \nb - Floresta do Congo \nc - Floresta da Indonésia \nd - Floresta da Nova Guiné", "a", "DICA: Está localizada na América do Sul.", "Qual é a maior floresta tropical do mundo? \n\na - Floresta Amazônica \nb - Floresta do Congo", "b"));
-        
-        perguntas.add(new PerguntaClass("Qual é o país mais populoso do mundo? \n\na - Índia \nb - Estados Unidos \nc - Indonésia \nd - China", "d", "DICA: Está localizado na Ásia.", "Qual é o país mais populoso do mundo? \n\na - Índia \nd - China", "a"));        
+        perguntas.add(new PerguntaClass("Qual é a maior floresta tropical do mundo? \n\na - Floresta Amazônica \nb - Floresta do Congo \nc - Floresta da Indonésia \nd - Floresta da Nova Guiné", "a", "DICA: Está localizada na América do Sul.", "Qual é a maior floresta tropical do mundo? \n\na - Floresta Amazônica \nb - Floresta do Congo", "b"));   
 
         repetidas = new ArrayList<>();
 
